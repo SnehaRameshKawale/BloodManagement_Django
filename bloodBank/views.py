16bbcf041c4a56a739b0d5e44f8c11dd5e59ef41
Sneha Kawale <sneharameshkawale2023@gmail.com>
2025-07-25 20:50:43 +0530

    Initial commit

--- bloodBank/views.py
+++ bloodBank/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request,'home.html',{'is_signup':False})
+
+def signup_view(request):
+    return render(request,'home.html',{'is_signup':True})
